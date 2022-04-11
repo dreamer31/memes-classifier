24f6301bafccefbce6b6c8a0db53486847ec290d
Tobal <jaramillo.cristobal.a@gmail.com>
2022-04-10 20:21:02 -0400

    categories names

---
+++ category.py
@@ -0,0 +1,34 @@
+categories = {
+
+
+    0: "No Tema",
+    1: "Sebastian Piñera",
+    2: "Michelle Bachelet",
+    3: "José Antonio Kast",
+    4: "Cathy Barriga",
+    5: "Gobierno",
+    6: "Parlamento",
+    7: "Corrupción",
+    8: "Crimen",
+    9: "Salud",
+    10: "Clima",
+    11: "Copa America",
+    12: "Deportes",
+    13: "Economia",
+    14: "Inmigración",
+    15: "Celebridades y entretención",
+    16: "Cultura",
+    17: "Curiosidades",
+    18: "Contenido Humano",
+    19: "Animales",
+    20: "Educación",
+    21: "AFP",
+    22: "Politica",
+    23: "Teconología y ciencia",
+    24: "Misceláneos",
+    25: "Comida",
+    26: "Estallido social",
+
+     
+
+}
