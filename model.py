dff526910e6da43c2a4e5f78bee6c36f24dd0ae5
Tobal <jaramillo.cristobal.a@gmail.com>
2022-04-10 21:33:24 -0400

    cnn model for 56x56

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=3, padding=1)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(1280, 512)
+        self.fc1 = nn.Linear(3920, 512)
         self.fc2 = nn.Linear(512, out_size)
 
     def forward(self, x):
@@ -27,24 +27,31 @@ class CNN(nn.Module):
         return x
 
 
-class CNN1(nn.Module): 
+class CNN_CATEGORY(nn.Module): 
 
-    def __init__(self):
-        super(CNN1, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
+    def __init__(self, out_classes):
+        super(CNN_CATEGORY, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(20, 30, kernel_size=3, padding=1)
+        self.max_pool1 = nn.MaxPool2d((3,3), stride=2, padding=1)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(720, 1024)
-        self.fc2 = nn.Linear(1024, 256)
+        self.fc1 = nn.Linear(1470, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.out = nn.Linear(256, out_classes)
 
     def forward(self, x):
-
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(self.max_pool1(self.conv1(x)))
+        x = F.relu(self.max_pool1(self.conv2_drop(self.conv2(x))))
+        x = F.relu(self.max_pool1(self.conv2_drop(self.conv3(x))))
         x = x.view(x.shape[0],-1)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
+        x = F.dropout(x, training=self.training)
+        x = self.fc3(x)
+        x = self.out(x)
         return x
 
 class ImageClassificator(nn.Module):
